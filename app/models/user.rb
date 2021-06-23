84f89cb7566c3e4ca8d9ae8ca6dd334437320cc4
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-06-23 10:06:34 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,35 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  validates_uniqueness_of :main_name
+  validates_presence_of :main_name
+  attr_writer :login
+
+  def login
+    @login || self.main_name || self.sub_name
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(main_name) = :value OR lower(sub_name) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
