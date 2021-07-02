80cfb76b03976fce542ec2882214a3be3fd06ee0
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-07-02 10:27:45 +0900

    新規topicのビュー作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,8 +3,23 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(
+      :topic_name, :creator
+    ).merge(user_id: current_user.id)
   end
 end
